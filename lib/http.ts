56d758221500ce0d437ba600cf037ae8108f08d7
Adam Griffin <ag@agriffin.me>
2025-01-21 14:14:08 -0800

    Added sleep function
    To slow fetch requests down in development so we get a better idea of how the app will deal with request latency.

--- lib/http.ts
+++ lib/http.ts
@@ -1,5 +1,6 @@
 import { objectToQueryString } from './listingSearchParams'
 import isEmpty from 'lodash/isEmpty'
+import { sleep } from '~/lib'
 
 export async function http<T = unknown>(
   url: string,
@@ -9,6 +10,11 @@ export async function http<T = unknown>(
   const urlWithParams = isEmpty(searchParams)
     ? url
     : `${url}?${objectToQueryString(searchParams)}`
+  // Slow things down a little in dev so we get a better idea of how the app
+  // will deal with request latency
+  if (process.env.NODE_ENV === 'development') {
+    await sleep(250)
+  }
   const res = await fetch(urlWithParams, options)
   if (!res.ok) {
     throw new Error(await res.text())
