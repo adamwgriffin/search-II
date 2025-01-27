9f8c27f2e5cad65c161228cdff94c91fd79c001f
Adam Griffin <ag@agriffin.me>
2025-01-26 19:15:54 -0800

    Refactored useUpdateFilters hook

--- hooks/useUpdateFilters.ts
+++ hooks/useUpdateFilters.ts
@@ -1,7 +1,5 @@
-import isEqual from 'lodash/isEqual'
-import omitBy from 'lodash/omitBy'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
-import { DefaultFilters, objectToQueryString } from '~/lib/listingSearchParams'
+import { getUpdatedQueryString } from '~/lib/listingSearchParams'
 import type { URLParams } from '~/types'
 
 export function useUpdateFilters() {
@@ -10,15 +8,10 @@ export function useUpdateFilters() {
   const searchParams = useSearchParams()
 
   return function (newParams: URLParams) {
-    const merged = { ...Object.fromEntries(searchParams), ...newParams }
-    const truthyValues = omitBy(merged, (value) => !value)
-    const defaultsRemoved = omitBy(truthyValues, (value, key) => {
-      return (
-        DefaultFilters.hasOwnProperty(key) &&
-        isEqual(DefaultFilters[key], value)
-      )
-    })
-    const updatedQueryString = objectToQueryString(defaultsRemoved)
+    const updatedQueryString = getUpdatedQueryString(
+      Object.fromEntries(searchParams),
+      newParams
+    )
     const url =
       updatedQueryString === '' ? pathname : `${pathname}?${updatedQueryString}`
     router.push(url)
--- lib/listingSearchParams.ts
+++ lib/listingSearchParams.ts
@@ -1,15 +1,50 @@
 import type { SearchParamsInit, URLParams } from '~/types'
+import isEqual from 'lodash/isEqual'
+import omitBy from 'lodash/omitBy'
 
 export const GeospatialSearchParams = ['bounds', 'boundary_id', 'zoom']
 
-export const DefaultFilters: URLParams = {
+/**
+ * Keep track of a subset of Listing Service param defaults so that we can avoid
+ * sending them in the request if the service would behave this way be default
+ * anyway
+ */
+export const ParamDefaults: Partial<URLParams> = Object.freeze({
+  page_index: '0',
+  page_size: '20',
   sort_by: 'listedDate',
   sort_direction: 'desc'
+})
+
+/**
+ * Remove params marked for removal as well as params that use default values.
+ * Setting a param to these falsey values indicates that it should be removed
+ * from the filters if present.
+ */
+export function removeUnwantedParams(params: URLParams) {
+  return omitBy(
+    params,
+    (value, key) =>
+      value === null || value === '' || isEqual(ParamDefaults[key], value)
+  )
 }
 
 export function objectToQueryString(params: URLParams) {
-  // Casting params as SearchParamsInit because the current type provided by Typescript for this is not correct
+  // Casting params as SearchParamsInit because the current type provided by
+  // Typescript for this is not correct
   return new URLSearchParams(params as SearchParamsInit)
     .toString()
     .replace(/%2C/g, ',') // Don't encode commas in url params
+}
+
+export function updateParams(currentParams: URLParams, newParams: URLParams) {
+  const merged = { ...currentParams, ...newParams }
+  return removeUnwantedParams(merged)
+}
+
+export function getUpdatedQueryString(
+  currentParams: URLParams,
+  newParams: URLParams
+) {
+  return objectToQueryString(updateParams(currentParams, newParams))
 }
