03f69db5049115b4df4483d81d082ec39e3ae9cc
Adam Griffin <ag@agriffin.me>
2025-06-25 17:32:52 -0700

    Make the logo link back to the home page

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,16 @@
 import { FaHouse } from "react-icons/fa6";
+import Link from "next/link";
 
 export function Logo() {
   return (
-    <div
-      className="flex gap-1 text-nowrap text-4xl font-semibold text-purple-500
-        dark:text-purple-300"
-    >
-      <FaHouse />
-      wsom
-    </div>
+    <Link href="/">
+      <div
+        className="flex gap-1 text-nowrap text-4xl font-semibold text-purple-500
+          dark:text-purple-300"
+      >
+        <FaHouse />
+        wsom
+      </div>
+    </Link>
   );
 }
