528bb4d1b729e45734de9ac1974d597063488ff9
Adam Griffin <ag@agriffin.me>
2025-07-18 14:57:22 -0700

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,14 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname
-});
+// This translates the older eslint config format that next uses to the new flat
+// config format
+const compat = new FlatCompat();
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next", "next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules/", ".next/", "dist/", "out/"]
+  },
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
