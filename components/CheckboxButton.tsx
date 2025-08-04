b4281f8614b372fbd7117b24832a4e82471b53d7
Adam Griffin <ag@agriffin.me>
2025-08-03 20:22:59 -0700

    Make checkbutton look better in dark mode

--- components/CheckboxButton.tsx
+++ components/CheckboxButton.tsx
@@ -10,9 +10,10 @@ export default function CheckboxButton({
     <label className="inline-block cursor-pointer">
       <input {...props} className="peer hidden" type="checkbox" />
       <span
-        className="inline-block px-4 py-2 rounded-md border text-sm font-medium transition-colors
-          bg-white border-gray-300 text-gray-800 hover:bg-gray-100
-          peer-checked:bg-blue-600 peer-checked:border-blue-700 peer-checked:text-white
+        className="inline-block px-4 py-2 rounded-md border text-sm font-medium
+          transition-colors bg-white dark:bg-gray-600 border-gray-300
+          text-inherit hover:bg-gray-100 peer-checked:bg-blue-600
+          peer-checked:border-blue-700 peer-checked:text-white
           peer-checked:hover:bg-blue-500"
       >
         {children}
