769b90186684904fc79454b1bb380296f38bbd34
Adam Griffin <ag@agriffin.me>
2025-08-06 12:32:12 -0700

    Prevent link from taking up full width

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,14 +3,15 @@ import Link from "next/link";
 
 export function Logo() {
   return (
-    <Link href="/">
-      <div
-        className="flex gap-1 text-nowrap text-4xl font-semibold text-purple-500
-          dark:text-purple-300"
+    <div>
+      <Link
+        href="/"
+        className="inline-flex gap-1 text-nowrap text-4xl font-semibold
+          text-purple-500 dark:text-purple-300"
       >
         <FaHouse />
         wsom
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
