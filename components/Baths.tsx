0444226e915ef424da213405f094990757c596e3
Adam Griffin <ag@agriffin.me>
2025-08-06 12:17:00 -0700

    Used InputButton for Baths

--- components/Baths.tsx
+++ components/Baths.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { InputButton } from "@/components/InputButton";
 import { useSearchState } from "@/providers/SearchStateProvider";
 
 const Values = [undefined, 1, 2, 3, 4, 5];
@@ -11,16 +12,17 @@ export function Baths() {
     <fieldset className="flex gap-2">
       <legend>Baths</legend>
       {Values.map((value) => (
-        <label key={`baths-${value ?? "any"}`} className="flex gap-1">
-          <input
-            type="radio"
-            name="baths"
-            value={value}
-            checked={searchState.baths_min === value}
-            onChange={() => setSearchState({ baths_min: value })}
-          />
+        <InputButton
+          key={`baths-${value ?? "any"}`}
+          type="radio"
+          name="baths"
+          value={value}
+          checked={searchState.baths_min === value}
+          onChange={() => setSearchState({ baths_min: value })}
+          className="rounded-full min-w-14 px-0"
+        >
           {value ? `${value}+` : "Any"}
-        </label>
+        </InputButton>
       ))}
     </fieldset>
   );
